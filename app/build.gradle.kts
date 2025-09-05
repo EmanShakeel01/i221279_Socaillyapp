891fd048b1c17aad6243ee5344bea25b73cb7475
Your Name <you@example.com>
2025-09-05 18:04:35 +0500

    Replicated activity page UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.google.android.material:material:1.9.0")
 
 }
